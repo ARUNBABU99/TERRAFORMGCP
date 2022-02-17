90441ce38dd410f59730e2ee9edb465d9e73ae97
ARUNBABU99 <arunbabu994724@gmail.com>
2022-02-17 14:34:57 +0530

    name change

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "terraform-network1"
 }
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance2"
+  name         = "terraform-instance3"
   machine_type = "f1-micro"
   zone         = "us-central1-c"
   boot_disk {
